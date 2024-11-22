d2e4c516b5594e39f958307dadec3b6043b7b9ec
tech publica <tech.publica@gmail.com>
2024-11-22 11:44:30 +0100

    updated dependencies

--- src/main/java/org/generation/italy/servlets/HelloWorldServlet.java
+++ src/main/java/org/generation/italy/servlets/HelloWorldServlet.java
@@ -1,4 +1,20 @@
 package org.generation.italy.servlets;
 
-public class HelloServlet {
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+
+@WebServlet("/hello")
+public class HelloWorldServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html");
+        response.getWriter().write(".....<h1>Hello, World!</h1>");
+    }
 }
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Welcome</title>
+</head>
+<body>
+    <h1>Welcome to My Web App</h1>
+    <p><%=java.time.LocalDateTime.now() %></p>
+    <a href="hello">Go to Hello Servlet!!</a>
+</body>
+</html>
